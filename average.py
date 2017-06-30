1b7cf0bd4aab2d9afc13d1a9ecda6fe3dc49d32f
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-06-30 07:24:05 -0400

    first commit

---
+++ average.py
@@ -0,0 +1,12 @@
+
+round1 = int(raw_input("Enter score for 1st round: "))
+
+round2 = int(raw_input("Enter score for 2nd round: "))
+
+round3 = int(raw_input("Enter score for 3rd round: "))
+   
+
+average = (round1 + round2 + round3) / 3
+
+
+print "the average of all scores is: ", average 
